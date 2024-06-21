35ba9a1b347a26e736a4a7ef23c9a2afd6d02a11
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-06-21 20:07:55 +0800

    feat: add crud functions in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json($product, 201);
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product, 200);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update($validated);
+
+        return response()->json($product, 200);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response('Deleted', 200);
+    }
 }
